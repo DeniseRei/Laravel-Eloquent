dc6653e62b62f494aba55ddb156615cc65f03f33
DeniseRei <lopesdeniserei@gmail.com>
2022-08-20 19:58:26 -0300

    consultas eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/select', function(){
+    //$users = User::all();
+    //$users = User::where('id', 10)->get();
+    //$users = User::first();
+    //$users = User::find(10);
+    //$users = User::findOrFail(10);
+    //$users = User::where('name', request('name'))->firstOrFail();
+    $users = User::firstWhere('name', request('name'));
+    dd($users);
+});
 
 Route::get('/', function () {
     return view('welcome');
