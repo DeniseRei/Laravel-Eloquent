68fd6c8ddf046de0ed117c50be875420d598c336
DeniseRei <lopesdeniserei@gmail.com>
2022-08-23 21:42:15 -0300

    paginação

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,6 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/select', function(){
@@ -26,7 +22,7 @@ Route::get('/select', function(){
 });
 
 Route::get('/where', function(User $user){
-   //$users = $user->get(); //trouxe a collection, todos os users
+   $users = $user->get(); //trouxe a collection, todos os users
    //$users = $user->where('email', '=', 'therese.buckridge@example.org')->first();
    //$users = $user->where('email','therese.buckridge@example.org')->first(); //não precisa passar o '='
    $filter = 'd';
@@ -36,16 +32,33 @@ Route::get('/where', function(User $user){
 /*     $users = $user->where('name', 'LIKE', "%{$filter}%")
                     ->whereName('Gabriel') //->whereNot(), ->whereDate(), ->whereIn('email', []), orWherein()
                     ->get(); */
-    $users = $user->where('name', 'LIKE', "%{$filter}%")
+    /* $users = $user->where('name', 'LIKE', "%{$filter}%")
                   ->orWhere(function($query) use ($filter){
                         $query->where('name', '<>', 'Denise');
                         $query->where('name', '=', $filter);
                   })
-                  ->toSql();
+                  ->toSql(); */
 
                 dd($users);
 
 });
+
+Route::get('/pagination', function () {
+    //$users = User::paginate(10);
+        //frontend = {{ $users->links() }}
+        //$users = User::where('name', 'LIKE', "%a%")->paginate();
+    //return $users;
+
+    //para API
+    $filter = request('filter');
+    $totalPage = request('paginate', 10);
+    $users = User::where('name', 'LIKE', "%{$filter}%")->paginate($totalPage);
+    //URL = http://localhost:8180/pagination?page=1&filter=a
+    //URL  http://localhost:8180/pagination?page=1&filter=a&paginate=20
+
+    return $users;
+});
+
 
 Route::get('/', function () {
     return view('welcome');
